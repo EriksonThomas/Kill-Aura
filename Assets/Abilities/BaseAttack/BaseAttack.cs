dca49bc00ce28ed511060273295edef98b9beaaf
chaseelliottpatterson <31286196+chaseelliottpatterson@users.noreply.github.com>
2022-06-03 10:08:42 -0600

    fixed double attack

--- Assets/Abilities/BaseAttack/BaseAttack.cs
+++ Assets/Abilities/BaseAttack/BaseAttack.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 public class BaseAttack : MonoBehaviour
-{
+{   
     public float attackDamage = 5.0f;
     void FixedUpdate()
     {
@@ -9,8 +10,10 @@ public class BaseAttack : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Enemy")
+        //TEMPORARY CHECK CAPSULE COLLIDER INSTEAD OF TAG
+        if (other.gameObject.tag == "Enemy" && other.GetType() == typeof(CapsuleCollider2D))
         {
+            Debug.Log(other.GetType());
             other.GetComponent<EnemyController>().DoDamage(attackDamage);
         }
     }
