15860d30502c5a66dd93d012b80fe2dca050f03c
chaseelliottpatterson <31286196+chaseelliottpatterson@users.noreply.github.com>
2022-06-03 11:12:56 -0600

    damage flashes

--- Assets/Enemy/Scripts/EnemyController.cs
+++ Assets/Enemy/Scripts/EnemyController.cs
@@ -4,51 +4,74 @@ public class EnemyController : MonoBehaviour
 {
     public float maxHealth = 10f;
     public float currentHealth = 10f;
-    private float dazedTime;
-    public float startDazedTime = 2;
+    private const float dazeTime = 0.4f;
+    public float dazeCounter = 0.0f;
     private float randomCount;
+    private SpriteRenderer my_sprite_renderer;
+    private Material white_mat;
+    private Material default_mat;
     public GameObject[] gemPrefab;
     public GameObject[] expPrefab;
     private int randomNumber;
     public float attackDamage;
-    void FixedUpdate()
+
+    void Start()
     {
-        if (currentHealth <= 0)
+        my_sprite_renderer = GetComponent<SpriteRenderer>();
+        white_mat = Resources.Load("damaged_material", typeof(Material)) as Material;
+        default_mat = my_sprite_renderer.material;
+    }
+    void FixedUpdate()
+    {       
+        if (dazeCounter > 0)
         {
-            for (float i = 0; i <= Random.Range(0, 5.0f); i++)
-            {
-                // Take a random number in Gem Prefab array and cast it to the spawn it in on death
-                randomNumber = Random.Range(0, gemPrefab.Length);
-                gameObject.transform.position = gameObject.transform.position + (Random.insideUnitSphere * .3f);
-                GameObject gem = Instantiate(gemPrefab[randomNumber], gameObject.transform.position, Quaternion.identity);
-            }
-
-            for (float i = 0; i <= Random.Range(0, 3.0f); i++)
-            {
-                // Take a random number in EXP Prefab array and cast it to the spawn it in on death
-                randomNumber = Random.Range(0, 2);
-                gameObject.transform.position = gameObject.transform.position + (Random.insideUnitSphere * .3f);
-                GameObject exp = Instantiate(expPrefab[0], gameObject.transform.position, Quaternion.identity);
-            }
-
-            Destroy(gameObject);
-        }
-
-        // Daze on damaged
-        if(dazedTime <= 0)
-        {
-            gameObject.GetComponent<EnemyMovement>().moveSpeed = gameObject.GetComponent<EnemyMovement>().moveSpeedStart;
+            gameObject.GetComponent<EnemyMovement>().moveSpeed = 0.0f;
+            dazeCounter -= Time.fixedDeltaTime;            
         }
         else
         {
-            gameObject.GetComponent<EnemyMovement>().moveSpeed = 0.0f;
-            dazedTime -= Time.fixedDeltaTime;
+            gameObject.GetComponent<EnemyMovement>().moveSpeed = gameObject.GetComponent<EnemyMovement>().moveSpeedStart;
         }
     }
     public void DoDamage(float damage)
     {
+        
+        currentHealth -= damage;  
         Debug.Log(gameObject.ToString() +" TOOK " + damage + " DAMAGE ");
-        dazedTime = startDazedTime;
-        currentHealth -= damage;       
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+            spawn_loot();
+        }
+        dazeCounter = dazeTime;
+        flash_white_mat();
+    }
+    
+    private void flash_white_mat()
+    {
+        my_sprite_renderer.material = white_mat;
+        Invoke("reset_material", dazeTime/2);
+    }
+    private void reset_material()
+    {
+        my_sprite_renderer.material = default_mat;
+    }
+    private void spawn_loot()
+    {
+        for (float i = 0; i <= Random.Range(0, 5.0f); i++)
+        {
+            // Take a random number in Gem Prefab array and cast it to the spawn it in on death
+            randomNumber = Random.Range(0, gemPrefab.Length);
+            gameObject.transform.position = gameObject.transform.position + (Random.insideUnitSphere * .3f);
+            GameObject gem = Instantiate(gemPrefab[randomNumber], gameObject.transform.position, Quaternion.identity);
+        }
+
+        for (float i = 0; i <= Random.Range(0, 3.0f); i++)
+        {
+            // Take a random number in EXP Prefab array and cast it to the spawn it in on death
+            randomNumber = Random.Range(0, 2);
+            gameObject.transform.position = gameObject.transform.position + (Random.insideUnitSphere * .3f);
+            GameObject exp = Instantiate(expPrefab[0], gameObject.transform.position, Quaternion.identity);
+        }
     }
 }
