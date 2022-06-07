2b4f76b75112a04fd83a8c9655bbf905ea90268b
Erikson <eriksonjthomas@gmail.com>
2022-06-06 19:02:03 -1000

    Damage Number bug fix

--- Assets/Abilities/BaseAttack/BaseAttack.cs
+++ Assets/Abilities/BaseAttack/BaseAttack.cs
@@ -15,9 +15,5 @@ public class BaseAttack : MonoBehaviour
         {
             other.GetComponent<EnemyController>().DoDamage(attackDamage);
         }
-        if (other.gameObject.tag == "Enemy")
-        {
-            other.GetComponent<EnemyController>().DoDamage(attackDamage);
-        }
     }
 }
