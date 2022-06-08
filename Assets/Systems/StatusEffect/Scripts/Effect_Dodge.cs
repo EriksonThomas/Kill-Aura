24579cd84769c87e1967c21a96a96f59020b367b
chaseelliottpatterson <31286196+chaseelliottpatterson@users.noreply.github.com>
2022-06-07 19:18:37 -0600

    Global Timer and Dodge fix

--- Assets/Systems/StatusEffect/Scripts/Effect_Dodge.cs
+++ Assets/Systems/StatusEffect/Scripts/Effect_Dodge.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Effects/Dodge", fileName = "Effect_Dodge")]
 public class Effect_Dodge : BaseEffect
 {
+    private float dodgeSpeedIncrease = 2.5f;
     public override bool Effect_IsDodging(bool originalDodging)
     {
         return true;
     }
-    
-    //speed up
-    //translucent
-    //collision off
+    public override void OnEffectStart()
+    {
+        GameHandler.instance.player.GetComponent<BoxCollider2D>().enabled = false;
+        GameHandler.instance.player.GetComponent<SpriteRenderer>().color = new Color(1,1,1,.35f);
+        GameHandler.instance.player.GetComponent<PlayerMovement>().moveSpeed = GameHandler.instance.player.GetComponent<PlayerMovement>().moveSpeed + dodgeSpeedIncrease;
+    }
+    public override void OnEffectEnd()
+    {
+        GameHandler.instance.player.GetComponent<BoxCollider2D>().enabled = true;
+        GameHandler.instance.player.GetComponent<SpriteRenderer>().color = new Color(1,1,1,1);
+        GameHandler.instance.player.GetComponent<PlayerMovement>().moveSpeed = GameHandler.instance.player.GetComponent<PlayerMovement>().moveSpeed - dodgeSpeedIncrease;
+
+    }
 }
