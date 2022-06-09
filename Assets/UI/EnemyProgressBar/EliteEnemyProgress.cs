140ef84df0f689cba75c57f3f2002168d0f8929a
Erikson <eriksonjthomas@gmail.com>
2022-06-09 08:12:47 -1000

    Progress Bars Fully Working
    Build and run game if bug occurs.

--- Assets/UI/EnemyProgressBar/EliteEnemyProgress.cs
+++ Assets/UI/EnemyProgressBar/EliteEnemyProgress.cs
@@ -11,6 +11,15 @@ public class EliteEnemyProgress : MonoBehaviour
     }
     void Update()
     {
+        if (slider.value <= slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+
+        if (slider.value  > slider.minValue && !fillImage.enabled)
+        {
+            fillImage.enabled = true;
+        }
 
         var playerProgress = GameHandler.instance.player.GetComponent<PlayerStats>().eliteEnemyMeter;
         var playerProgressNeeded = WaveManager.instance.GetComponent<WaveManager>().eliteEnemyMeterMax;
